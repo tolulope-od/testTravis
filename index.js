7737415cce5c46f1d9bd3a71ce3314a731f0a867
Tolulope Odueke <tolulope.od@gmail.com>
2019-02-12 14:34:06 +0100

    Change index file

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ const PORT = 8080;
 // configure the app to listen on localhost
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server Running on port ${PORT}`);
+  console.log(`Server is up and Running on port ${PORT}`);
 });
